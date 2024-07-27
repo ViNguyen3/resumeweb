c180f8fbeb39a84413e5dfccd3cb6554fb35f4f1
ViNguyen3 <80879169+ViNguyen3@users.noreply.github.com>
2024-07-27 01:56:29 -0700

    fixed
    careful the difference between className and classname

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,9 @@
 import {useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap"; 
-import {ArrowRightCircle} from "react-bootstrap-icons"; 
+import {App, ArrowRightCircle} from "react-bootstrap-icons"; 
 import headerImg from "../assets/images/header-img.svg"; 
+import TrackVisibility from 'react-on-screen';
+
 
 export const Banner = () => {
 
@@ -44,14 +46,19 @@ export const Banner = () => {
     }
 
     return(
-        <section classname= "banner" id="home">
+        <section className= "banner" id="home">
             <Container>
                 <Row className="align-items-center"> 
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Resume</span>
-                        <h1>{'Hi '}<span className="wrap">{text}</span></h1>     
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>               
-                        <button onClick={() => console.log('connect')}>Let's Connect<ArrowRightCircle size={25}></ArrowRightCircle></button>
+                    <TrackVisibility>
+                    {({ isVisible }) =>
+                        <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                            <span className="tagline">Welcome to my Resume</span>
+                            <h1>{'Hi '}<span className="wrap">{text}</span></h1>     
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>               
+                            <button className="my-button" onClick={() => console.log('connect')}>Let's Connect<ArrowRightCircle size={25}></ArrowRightCircle></button>
+                        </div>}
+                    </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header img"/> 
