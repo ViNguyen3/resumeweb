0b0193e71e65a25d9ca205c269cdf98bd3fa8294
Lunastra <Vi.Nguyen03@student.csulb.edu>
2024-08-19 20:09:46 -0700

    update the project page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { NavBar } from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Banner} from "./components/Banner";
 import {Skills} from "./components/Skills";
-
+import {Projects} from "./components/Projects";
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
